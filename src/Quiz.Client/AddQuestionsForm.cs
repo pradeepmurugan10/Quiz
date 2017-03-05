602179b797c6652ad599dca360ed87416e6f42f8
Pradeep Murugan <Pradeep Murugan@DESKTOP-I1K4G0Q>
2017-03-06 00:52:13 +0530

    Add Questions Form added

---
+++ src/Quiz.Client/AddQuestionsForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using Quiz.Common.Models;
+using LiteDB;
+
+namespace Quiz.Client
+{
+    public partial class AddQuestionsForm : MetroForm
+    {
+        private IEnumerable<Question> question;
+        private LiteDatabase _db;
+       
+        public AddQuestionsForm()
+        {
+            InitializeComponent();
+
+        }
+        private void AddQuestion_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
